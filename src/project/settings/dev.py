98f0b5bd102bcb3d1e09feaa838485abd4b7d998
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2021-03-17 11:04:47 +0100

    Release v0.1.3

--- src/project/settings/dev.py
+++ src/project/settings/dev.py
@@ -5,31 +5,28 @@ import six
 
 from .base import *  # noqa
 
-os.environ['RELATIVE_SETTINGS_MODULE'] = '.dev'
-ALLOWED_HOSTS = ['*']
+os.environ["RELATIVE_SETTINGS_MODULE"] = ".dev"
+ALLOWED_HOSTS = ["*"]
 CORS_ORIGIN_ALLOW_ALL = True
 DEBUG_TOOLBAR_CONFIG = {
-    'SHOW_TOOLBAR_CALLBACK': lambda x: True,
+    "SHOW_TOOLBAR_CALLBACK": lambda x: True,
 }
 DEBUG = True
 SWAGGER_ENABLED = DEBUG
 
-INSTALLED_APPS += (
-    'debug_toolbar',
-)
+INSTALLED_APPS += ("debug_toolbar",)
 
-MIDDLEWARE += (
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-)
+MIDDLEWARE += ("debug_toolbar.middleware.DebugToolbarMiddleware",)
 
-MEDIA_URL = '/media/'
+
+MEDIA_URL = "/media/"
 
 # Force every loggers to use console handler only. Note that using 'root'
 # logger is not enough if children don't propage.
-for logger in six.itervalues(LOGGING['loggers']):  # noqa
-    logger['handlers'] = ['console']
+for logger in six.itervalues(LOGGING["loggers"]):  # noqa
+    logger["handlers"] = ["console"]
 # Log every level.
-LOGGING['handlers']['console']['level'] = logging.NOTSET  # noqa
+LOGGING["handlers"]["console"]["level"] = logging.NOTSET  # noqa
 
 try:
     from .local import *  # noqa
