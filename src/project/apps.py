647b85880549b5c48929543cddec2ef44ac739cf
Jeremie Pardou-Piquemal <571533+jrmi@users.noreply.github.com>
2021-04-24 10:38:51 +0200

    Add terra-opp permissions

--- src/project/apps.py
+++ src/project/apps.py
@@ -1,4 +1,5 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class ProjectConfig(AppConfig):
@@ -6,3 +7,15 @@ class ProjectConfig(AppConfig):
 
     def ready(self):
         from . import receivers  # NOQA
+
+
+# Add translation here to avoid missing translation
+# in terra_account module.
+# FIXME
+# Translation from terra-opp...
+extra_translations = [
+    _("Can manage viewpoints"),
+    _("Can manage pictures"),
+    _("Can add pictures"),
+    _("Can manage campaign"),
+]
